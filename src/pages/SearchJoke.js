b24b36644d68112fd573a604f6d502ed534f4134
Nate Crawford <nathanwcrawford22@gmail.com>
2022-10-31 12:13:57 -0400

    Minor Cleanup

--- src/pages/SearchJoke.js
+++ src/pages/SearchJoke.js
@@ -3,8 +3,6 @@ import SearchForJokes from "../components/SearchForJokes";
 
 export default function SearchJoke() {
 
-    // const searchApi = `https://icanhazdadjoke.com/search?term=${searchTerm}`;
-
     return (
         <div>
             <NavBar />
